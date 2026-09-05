ae66d6f8ff4efe513e35d7c658740e096daa7402
Abhinav TIwari <abhinavtiwari021@gmail.com>
2026-09-05 23:30:16 +0530

    Remove empty line in jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,7 +5,6 @@ const createJestConfig = nextJest({
     dir: './',
 })
 
-
 const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
